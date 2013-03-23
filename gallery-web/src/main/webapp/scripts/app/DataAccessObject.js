5953f755b3821999290faaa5a4aa47b6a8337695
David Knezic <davidknezic@gmail.com>
2013-03-23 12:29:34 +0100

    images.random, save & publish in DataAccessObject

--- gallery-web/src/main/webapp/scripts/app/DataAccessObject.js
+++ gallery-web/src/main/webapp/scripts/app/DataAccessObject.js
@@ -15,6 +15,26 @@ define(['jquery'], function ($) {
 		});
 	};
 	
+	this._ajaxPost = function (url, data, callback) {
+		$.ajax({
+			url: url,
+			data: data,
+			dataType: 'json',
+			type: 'POST',
+			contentType : 'application/json',
+			error: function (jqXhr, textStatus, errorThrown) {
+				if (typeof(callback) === 'function') {
+					callback({ error: undefined });
+				}
+			},
+			success: function (data, textStatus, jqXhr) {
+				if (typeof(callback) === 'function') {
+					callback(data);
+				}
+			}
+		});
+	};
+	
 	this._imageList = function (count, startId, callback) {
 		var data = {};
 		
@@ -23,12 +43,29 @@ define(['jquery'], function ($) {
 		}
 		
 		_ajax('api/images/list/' + count, data, callback);
-	}
+	};
+	
+	this._randomImages = function (count, callback) {
+		_ajax('api/images/random/' + count, {}, callback);
+	};
+	
+	this._saveImage = function (image) {
+		_ajaxPost('api/images/edit', JSON.stringify(image));
+	};
+	
+	this._saveAndPublishImage = function (image) {
+		_ajaxPost('api/images/edit', JSON.stringify(image), function (data) {
+			_ajaxPost('api/images/publish', { id: data.id });
+		});
+	};
 	
 	// exports
 	return {
 		images: {
-			list: this._imageList
+			list: this._imageList,
+			random: this._randomImages,
+			save: this._saveImage,
+			publish: this._saveAndPublishImage
 		}
 	};
 });
