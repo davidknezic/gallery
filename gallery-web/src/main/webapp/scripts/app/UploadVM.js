6f3310fb4726d8f18327bb6b59b6b8899e240c33
David Knezic <davidknezic@gmail.com>
2013-03-23 13:57:22 +0100

    Implement working drop-zone upload

--- gallery-web/src/main/webapp/scripts/app/UploadVM.js
+++ gallery-web/src/main/webapp/scripts/app/UploadVM.js
@@ -1,17 +1,40 @@
 // Upload view model class
 
-define(['knockout', 'binding/uploader'], function (ko) {
+define(['knockout', 'app/DataAccessObject', 'binding/uploader'], function (ko, dao) {
 	return function () {
 		var self = this;
 		
 		self.images = ko.observableArray();
 		
-		self.uploadImage = function () {
-			
+		self.imageCount = ko.computed(function () {
+			return self.images().length;
+		});
+		
+		self.hasImages = ko.computed(function () {
+			return self.images().length != 0;
+		});
+		
+		self.uploadImage = function () {	
 		};
 		
 		self.uploadedImage = function (image) {
-			self.images.push(image);
+			self.images.push({
+				id: image.id,
+				filename: ko.observable(image.filename),
+				comment: ko.observable(image.comment)
+			});
+		};;
+		
+		self.publishImage = function (image) {
+			var toSave = {
+				id: image.id,
+				filename: image.filename(),
+				comment: image.comment()
+			};
+			
+			dao.images.publish(toSave, function (data) {
+				self.images.remove(image);
+			});
 		};
 	};
 });
