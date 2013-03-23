2e33351136ba7162b31e10f567cb8bd8867b5f48
David Knezic <davidknezic@gmail.com>
2013-03-23 12:34:38 +0100

    No fake image comment anymore, BrowseVM cleanup

--- gallery-web/src/main/webapp/scripts/app/BrowseVM.js
+++ gallery-web/src/main/webapp/scripts/app/BrowseVM.js
@@ -6,8 +6,6 @@ define(['jquery', 'knockout', 'binding/infiniteScroll', 'app/DataAccessObject'],
 		
 		self.images = ko.observableArray();
 		
-		//dao.images.list(20, undefined, self.images);
-		
 		self.load = function (reactivate) {
 			var lastIndex = self.images().length - 1;
 			var last = self.images()[lastIndex];
@@ -16,14 +14,10 @@ define(['jquery', 'knockout', 'binding/infiniteScroll', 'app/DataAccessObject'],
 			
 			dao.images.list(20, lastId, function (data) {
 				$.each(data, function (i, o) {
-					o.comment = "Na nu was ist denn das für ein schönes hübsches Bildchen hier? Mal was schönes für die Augen!";
-					o.comment += o.comment;
-					o.comment += o.comment;
 					self.images.push(o);
 				});
 				reactivate();
 			});
-			
-		}
+		};
 	};
 });
