e826a3505cdf5159cd35932bfe4126b4862f0691
David Knezic <davidknezic@gmail.com>
2013-04-09 11:15:29 +0200

    Save uploads to local storage until published

--- gallery-web/src/main/webapp/scripts/app/UploadVM.js
+++ gallery-web/src/main/webapp/scripts/app/UploadVM.js
@@ -1,10 +1,23 @@
 // Upload view model class
 
-define(['knockout', 'app/DataAccessObject', 'binding/uploader', 'binding/dropzone'], function (ko, dao) {
+define(['jquery', 'knockout', 'app/DataAccessObject', 'binding/uploader', 'binding/dropzone'], function ($, ko, dao) {
 	return function () {
 		var self = this;
 		
 		self.images = ko.observableArray();
+		
+		var storedImages = localStorage.getItem("uploads");
+		if (storedImages !== undefined) {
+			var arr = $.parseJSON(storedImages);
+			
+			$.each(arr, function(index, image) {
+				self.images.push({
+					id: image.id,
+					filename: ko.observable(image.filename),
+					comment: ko.observable(image.comment)
+				});
+			});
+		}
 		
 		self.imageCount = ko.computed(function () {
 			return self.images().length;
@@ -23,6 +36,18 @@ define(['knockout', 'app/DataAccessObject', 'binding/uploader', 'binding/dropzon
 				filename: ko.observable(image.filename),
 				comment: ko.observable(image.comment)
 			});
+			
+			var images = [];
+			
+			$.each(self.images(), function(index, value) {
+				images.push({
+					id: value.id,
+					filename: value.filename(),
+					comment: value.comment()
+				});
+			});
+			
+			localStorage.setItem("uploads", JSON.stringify(images));
 		};;
 		
 		self.publishImage = function (image) {
